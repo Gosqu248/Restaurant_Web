7903f02b0d47231d0e8a94d64abbf83229f261e5
Gosqu248 <grzegorzurban248@gmail.com>
2025-01-15 12:26:01 +0100

    Added login with 2FA

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NgIf} from '@angular/common';
+import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
     ReactiveFormsModule,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  showPassword: boolean = false; // Password visibility toggle
+  showPassword: boolean = false;
   loginForm: FormGroup;
+  is2FA: boolean = false;
+  isLoginError: boolean = false;
+  errorMessage: string | null = null;
+  twoFactorCode: string = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -29,11 +38,53 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Login Form Submitted', this.loginForm.value);
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      this.is2FA = false;
+
+      this.authService.login(email, password)
+        .subscribe({
+          next: (isAuthenticated: boolean) => {
+            if (isAuthenticated) {
+              this.is2FA = true;
+            } else {
+              console.log("Nieprawidłowy login lub hasło");
+            }
+          },
+          error: (error) => {
+            if (error.message === 'Account is locked. Try again later.') {
+              console.error('Konto jest zablokowane, spróbuj później.');
+              this.isLoginError = true;
+              this.errorMessage = 'locked';
+            } else {
+              console.error('Login error: ', error);
+              this.isLoginError = true;
+            }
+          }
+        });
     }
   }
 
   loginWithGoogle() {
     window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  }
+
+  verify2FA(code: string) {
+    this.authService.verify2FA(code).subscribe({
+      next: (isAuthenticated: boolean) => {
+        if (isAuthenticated) {
+          console.log("Logged in successfully");
+          this.authService.loginEvent.emit();
+          this.router.navigate(['/'])
+        } else {
+          console.error("Invalid 2FA code");
+          this.isLoginError = true;
+        }
+      },
+      error: (error) => {
+        console.error('2FA error: ', error);
+        this.isLoginError = true;
+      }
+    });
   }
 }
