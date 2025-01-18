d39dd51383cf8191e6033861d3693a6c0f043f77
Gosqu248 <grzegorzurban248@gmail.com>
2025-01-18 17:50:10 +0100

    Change groupedMenus

--- src/app/components/menu/menu-main/menu-main.component.ts
+++ src/app/components/menu/menu-main/menu-main.component.ts
@@ -5,10 +5,12 @@ import configs from '@tsparticles/configs';
 import {MenuService} from '../../../services/menu.service';
 import {NgParticlesService, NgxParticlesModule} from '@tsparticles/angular';
 import {loadSlim} from '@tsparticles/slim';
-import {NgForOf} from '@angular/common';
+import {KeyValuePipe, NgForOf} from '@angular/common';
 import {MenuItemComponent} from '../menu-item/menu-item.component';
 import {MenuCategoryComponent} from '../menu-category/menu-category.component';
 import {Subject, takeUntil} from 'rxjs';
+import {FormsModule} from '@angular/forms';
+import {GroupedMenuItems} from '../../../interfaces/grouped-menu-items';
 
 @Component({
   selector: 'app-menu-main',
@@ -16,15 +18,21 @@ import {Subject, takeUntil} from 'rxjs';
     NgForOf,
     NgxParticlesModule,
     MenuItemComponent,
-    MenuCategoryComponent
+    MenuCategoryComponent,
+    FormsModule,
+    KeyValuePipe
   ],
   templateUrl: './menu-main.component.html',
   standalone: true,
   styleUrl: './menu-main.component.scss'
 })
 export class MenuMainComponent implements OnInit, OnDestroy {
-  filteredMenus: Menu[] = [];
+  menus: Menu[] = [];
   private destroy$ = new Subject<void>();
+  searchMenuItem: string = '';
+  filteredMenus: Menu[] = [];
+  groupedMenuItems: GroupedMenuItems[] = [];
+
 
   id = "tsparticles";
   particlesOptions: ISourceOptions = {
@@ -51,7 +59,10 @@ export class MenuMainComponent implements OnInit, OnDestroy {
   loadMenus(): void {
     this.menuService.filteredMenus$
       .pipe(takeUntil(this.destroy$))
-      .subscribe((menus) => this.filteredMenus = menus);
+      .subscribe((menus) => {
+        this.menus = menus;
+        this.groupMenuItems(menus);
+      });
   }
 
   generateParticles(): void {
@@ -60,5 +71,32 @@ export class MenuMainComponent implements OnInit, OnDestroy {
     });
   }
 
+  filterMenus() {
+    if (this.searchMenuItem !== '') {
+      const filteredMenus = this.menus.filter((menu) => {
+        return menu.name.toLowerCase().includes(this.searchMenuItem.toLowerCase());
+      });
+      this.groupMenuItems(filteredMenus);
+    } else {
+      this.groupMenuItems(this.menus);
+    }
+  }
 
+  private groupMenuItems(menus: Menu[]) {
+    const groupedMap = new Map<string, Menu[]>();
+
+    menus.forEach((menu) => {
+      if (!groupedMap.has(menu.category)) {
+        groupedMap.set(menu.category, []);
+      }
+      groupedMap.get(menu.category)?.push(menu);
+    });
+
+    this.groupedMenuItems = Array.from(groupedMap.entries())
+      .map(([category, items]) => ({
+        category,
+        items: items.sort((a, b) => a.name.localeCompare(b.name))
+      }))
+      .sort((a, b) => a.category.localeCompare(b.category));
+  }
 }
